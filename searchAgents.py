d0cbfe99bb7f644cdeaee37845ea990240c59dc2
ngkhn <2059023@itec.hcmus.edu.vn>
2023-03-02 10:54:15 +0700

    Update searchAgents.py

--- searchAgents.py
+++ searchAgents.py
@@ -2,7 +2,9 @@ import random
 
 from game import Agent
 from game import Directions
-
+import time
+import search
+import problems
 
 class GoWestAgent(Agent):
     def getAction(self, state):
@@ -30,6 +32,17 @@ class SearchAgent(Agent):
         state: a GameState object (pacman.py)
         """
         # TODO 11
+        
+        if self.searchFunction == None: 
+            raise Exception("No search function provided for SearchAgent")
+        starttime = time.time()
+        problem = self.searchType(state) # Makes a new search problem
+        self.actions  = self.searchFunction(problem) # Find a path
+        totalCost = problems.getCostOfActions(self.actions)
+        print('Path found with total cost of %d in %.1f seconds' % (totalCost, time.time() - starttime))
+        if '_expanded' in dir(problem): 
+            print('Search nodes expanded: %d' % problems._expanded)
+
 
     def getAction(self, state):
         """
@@ -40,23 +53,47 @@ class SearchAgent(Agent):
         state: a GameState object (pacman.py)
         """
         # TODO 12
+        
+        if 'actionIndex' not in dir(self): self.actionIndex = 0
+        i = self.actionIndex
+        self.actionIndex += 1
+        if i < len(self.actions):
+            return self.actions[i]
+        else:
+            return Directions.STOP
 
 
 class BFSFoodSearchAgent(SearchAgent):
     # TODO 13
+    
+    def __init__(self):
+        self.searchFunction = search.breadthFirstSearch
+        self.searchType = problems.SingleFoodSearchProblem
     pass
 
 
 class DFSFoodSearchAgent(SearchAgent):
     # TODO 14
+    
+    def __init__(self):
+        self.searchFunction = search.depthFirstSearch
+        self.searchType = problems.SingleFoodSearchProblem
     pass
 
 
 class UCSFoodSearchAgent(SearchAgent):
     # TODO 15
+    
+    def __init__(self):
+        self.searchFunction = search.uniformCostSearch
+        self.searchType = problems.SingleFoodSearchProblem
     pass
 
 
 class AStarFoodSearchAgent(SearchAgent):
     # TODO 16
+    
+    def __init__(self):
+        self.searchFunction = search.aStarSearch
+        self.searchType = problems.MultiFoodSearchProblem
     pass
